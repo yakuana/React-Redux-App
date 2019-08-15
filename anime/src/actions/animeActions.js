a6a69db9d04f05603077acb3ce1db0792896fbc3
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-15 14:10:34 -0500

    WIP: Created App called Anime. Added actions, components and reducers folders.

---
+++ anime/src/actions/animeActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const FETCH_ANIME_DATA_START = 'FETCH_ANIME_DATA_START';
+export const FETCH_ANIME_DATA_SUCCESS = 'FETCH_ANIME_DATA_SUCCESS';
+export const FETCH_ANIME_DATA_FAILURE = 'FETCH_ANIME_DATA_FAILURE';
+
+export const getAnimeData = () => {
+    return dispatch => {
+      dispatch({ type: FETCH_ANIME_DATA_START });
+      axios
+        .get('https://api.oceandrivers.com:443/v1.0/getWebCams/')
+        .then(response => {
+            // successful 
+            console.log("response data", response.data)
+            // dispatch({ type: FETCH_ANIME_DATA_SUCCESS, payload: res.data.data });
+        })
+        .catch(error => {
+            // unsuccessful 
+            dispatch({ type: FETCH_ANIME_DATA_FAILURE, payload: error.response });
+        });
+    };
+  };
